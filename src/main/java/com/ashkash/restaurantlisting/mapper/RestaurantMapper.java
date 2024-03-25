63cd44e6f4aec765d32a27446d94da810d005d14
Ashwin-hp <88140600+Ashwin-hp@users.noreply.github.com>
2024-03-25 23:32:38 +0530

    commit messsage

---
+++ src/main/java/com/ashkash/restaurantlisting/mapper/RestaurantMapper.java
@@ -0,0 +1,17 @@
+package com.ashkash.restaurantlisting.mapper;
+
+import com.ashkash.restaurantlisting.dto.RestaurantDTO;
+import com.ashkash.restaurantlisting.entity.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);
+
+    RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);
+
+}
